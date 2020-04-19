7a0064d68857a2f5219bd939e17d9f4a19804ab6
sudeep-katiar <chandanpro32@gmail.com>
2020-04-19 20:03:16 +0530

    [sudeep-katiar].modified user service

--- src/app/service/userservice.service.ts
+++ src/app/service/userservice.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+
+  private queryParam = new Subject<any>();
   private API_URL = environment.apiUrl;
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -22,6 +24,14 @@ export class UserServiceService {
   login(user: any): Observable<any> {
     console.log(user);
     return this.http.post<any>(this.API_URL + environment.loginURL, user, this.httpOptions);
+  }
+
+  setQueryParam(message: any) {
+    this.queryParam.next({ id: message });
+  }
+
+  getQueryParam(): Observable<any> {
+    return this.queryParam.asObservable();
   }
   
 }
