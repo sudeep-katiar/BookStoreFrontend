1f8b1127fc25c7282b86e69e6ff7b026265618ba
sudeep-katiar <chandanpro32@gmail.com>
2020-04-19 20:04:14 +0530

    [sudeep-katiar].added book model

---
+++ src/app/model/books.model.ts
@@ -0,0 +1,12 @@
+export class Book {
+    bookId: number;
+    bookName: string;
+    bookCode: string;
+    bookDetails: string;
+    price: number;
+    quantity: number;
+    authorName : string;
+    wishlist : boolean;
+    addToBag : boolean;
+    createdBy : number;
+  }
