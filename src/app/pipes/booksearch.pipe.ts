a9813cc5e6f54a72ea339aa1745bc2b0cac3d2ff
sudeep-katiar <chandanpro32@gmail.com>
2020-04-24 14:06:04 +0530

    [sudeep-katiar].booksearch.pipe.ts

--- src/app/pipes/booksearch.pipe.ts
+++ src/app/pipes/booksearch.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Book } from '../model/books.model';
 
 @Pipe({
-  name: 'booksearch'
+  name: 'bookSearch',
+  pure: false
 })
 export class BooksearchPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
-  }
+  private counter = 0;
 
+  transform(books: Book[], searchTerm: string): Book[] {
+    if (!books || !searchTerm) {
+      return books;
+    }
+    return books.filter(book =>
+      book.bookName.toLowerCase().indexOf(searchTerm.toLowerCase()) != -1);
+  }
 }
