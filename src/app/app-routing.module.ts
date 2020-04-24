a1ea6f84abd89907f64b96b997ed84146f11c971
sudeep-katiar <chandanpro32@gmail.com>
2020-04-24 14:04:19 +0530

    [sudeep-katiar].app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
-  { path: 'dashboard', component:DashboardComponent }
+  { path: 'dashboard', component:DashboardComponent },
+  { path: 'cart', component:CartComponent }
 ];
 
 @NgModule({
